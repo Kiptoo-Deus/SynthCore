a4772a246a57510fde03e1605c56631bf12e5892
Joel Kiptoo <deusjoel150@gmail.com>
2026-03-18 20:50:15 +0300

    Refine core type definitions and audio buffer layout

--- core/include/synthcore/Types.hpp
+++ core/include/synthcore/Types.hpp
@@ -6,6 +6,7 @@ namespace synthcore {
 
 using Sample = float;
 using SampleRate = double;
+using MidiNote = uint8_t;
 
 constexpr int MAX_VOICES = 64;
 constexpr int MAX_ELEMENTS_PER_VOICE = 8;
@@ -13,16 +14,9 @@ constexpr int MAX_POLYPHONY = 64;
 
 constexpr double A4_FREQUENCY = 440.0;
 
-struct MidiNote {
-    uint8_t note;
-    uint8_t velocity;
-    uint8_t channel;
-};
-
 struct AudioBuffer {
-    Sample* left;
-    Sample* right;
-    int numFrames;
+    Sample data[8192];
+    size_t size;
 };
 
 } // namespace synthcore
