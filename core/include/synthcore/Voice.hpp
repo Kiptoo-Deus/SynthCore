e8f2f4fa63de5615f780432e95983dc577e5cf89
Joel Kiptoo <deusjoel150@gmail.com>
2026-03-18 20:52:02 +0300

    Add multi-element voice structure and state

--- core/include/synthcore/Voice.hpp
+++ core/include/synthcore/Voice.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "synthcore/Types.hpp"
+#include "synthcore/Element.hpp"
+#include "synthcore/SampleMap.hpp"
+#include <array>
 
 namespace synthcore {
 
 class Voice {
 public:
-    Voice() = default;
+    Voice();
     ~Voice() = default;
 
     Voice(const Voice&) = delete;
@@ -15,7 +18,23 @@ public:
     Voice(Voice&&) noexcept = default;
     Voice& operator=(Voice&&) noexcept = default;
 
+    void noteOn(const SampleMap& sampleMap, MidiNote note, float velocity, SampleRate sampleRate) noexcept;
+    void noteOff() noexcept;
+    void reset() noexcept;
+    
+    Sample process(SampleRate sampleRate) noexcept;
+    
+    bool isActive() const noexcept;
+    MidiNote getNote() const noexcept { return note_; }
+    bool isNoteOn() const noexcept { return noteOn_; }
+    
+    Element& getElement(size_t index) noexcept { return elements_[index]; }
+
 private:
+    std::array<Element, MAX_ELEMENTS_PER_VOICE> elements_;
+    MidiNote note_;
+    bool noteOn_;
+    bool active_;
 };
 
 } // namespace synthcore
